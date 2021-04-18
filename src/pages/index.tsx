439cfcaead9b3ca780e201c0f01ba06218dde6d1
Rafael Antunes <rafaelmessias710@gmail.com>
2021-04-18 19:02:06 -0400

    feat: fix clean up index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import Head from "next/head";
-import { QuestionGenerator } from "utils/question-generator";
+import { InferGetStaticPropsType } from "next";
 
 import countries from "data/countries.json";
 import languages from "data/languages.json";
 import continents from "data/continents.json";
 
-import { InferGetStaticPropsType } from "next";
+import { Container } from "ui/components/Layout";
+import { Modal } from "ui/components/Modal";
+import { QuestionsProvider } from "ui/contexts/QuestionsContext";
+import { QuestionGenerator } from "utils/question-generator";
+const questionGenerator = new QuestionGenerator({ countries, languages });
 
 export const getStaticProps = async () => {
+  const questions = [
+    questionGenerator.capitalOf(),
+    questionGenerator.flagOf(),
+    questionGenerator.languageOf(),
+  ];
+
   return {
     props: {
-      countries,
-      languages,
-      continents,
+      questions,
     },
   };
 };
@@ -20,91 +28,13 @@ export const getStaticProps = async () => {
 export default function Home(
   props: InferGetStaticPropsType<typeof getStaticProps>
 ) {
-  const questionGenerator = new QuestionGenerator({
-    countries: props.countries,
-    languages: props.languages,
-  });
-  // console.log([
-  //   questionGenerator.capitalOf(),
-  //   questionGenerator.flagOf(),
-  //   questionGenerator.languageOf(),
-  // ]);
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{" "}
-          <a className="text-[#ccc]" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+    <div className="min-h-screen bg-blue-400">
+      <Container>
+        <QuestionsProvider questions={props.questions}>
+          <Modal />
+        </QuestionsProvider>
+      </Container>
     </div>
   );
 }
