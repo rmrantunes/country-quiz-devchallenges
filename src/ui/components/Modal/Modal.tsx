237749ba8a93d0e181d986cac26e2abc8d4d948d
Rafael Antunes <rafaelmessias710@gmail.com>
2021-04-18 19:03:13 -0400

    feat: add modal

---
+++ src/ui/components/Modal/Modal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { QuestionModal } from "./QuestionModal";
+
+export const Modal = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-white mb-2">COUNTRY QUIZ</h1>
+      <QuestionModal />
+    </div>
+  );
+};
