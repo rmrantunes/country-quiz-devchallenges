96413ebb1a737130e4d548cb09a321dd6ebc8dfd
Rafael Antunes <rafaelmessias710@gmail.com>
2021-04-18 19:02:37 -0400

    feat: add some tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/components/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "dark-blue": "#2F527B",
+        mustard: "#F9A826",
+      },
+      spacing: {
+        116: "29rem",
+      },
+    },
   },
   variants: {
     extend: {},
