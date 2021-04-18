4d931af9b951c33c5cc329a81f392b5cc71ed705
Rafael Antunes <rafaelmessias710@gmail.com>
2021-04-18 15:01:25 -0400

    feat: new selectLanguage method

--- src/utils/question-generator.ts
+++ src/utils/question-generator.ts
@@ -1,5 +1,6 @@
 import { Question } from "types/question";
 import { Country, MappedCountry, Source } from "types/question-source";
+import { getRandomArrayIndex } from "./app-utils";
 
 export class QuestionGenerator {
   constructor(private source: Source) {}
@@ -8,15 +9,21 @@ export class QuestionGenerator {
     const selectedCountries = [];
     const { countries } = this.source;
     for (let i = 0; i < 4; i++) {
-      const countriesRandomIndex = Math.floor(Math.random() * countries.length);
+      const countriesRandomIndex = getRandomArrayIndex(countries);
       selectedCountries.push(countries[countriesRandomIndex]);
     }
     return selectedCountries;
   }
 
   private selectCorrectCountry(countries: MappedCountry[]) {
-    const countriesRandomIndex = Math.floor(Math.random() * countries.length);
+    const countriesRandomIndex = getRandomArrayIndex(countries);
     return countries[countriesRandomIndex];
+  }
+
+  private selectLanguage() {
+    const { languages } = this.source;
+    const languagesRandomIndex = getRandomArrayIndex(languages);
+    return languages[languagesRandomIndex];
   }
 
   capitalOf(): Question {
@@ -50,6 +57,12 @@ export class QuestionGenerator {
       title: "Which country does this flag belongs to?",
       correctAnswer: correctCountry.name,
       possibleAnswers: countriesNameAndCode.map(({ name }) => name),
+    };
+  }
+
+  language(): Question {
+    return {
+      type: "language",
     };
   }
 }
