7efeb11be912e7ffc6e0b3ac468e1ec4ca2fdcc6
Rafael Antunes <rafaelmessias710@gmail.com>
2021-04-18 21:12:09 -0400

    feat: add question modal component

---
+++ src/ui/components/Modal/QuestionModal.tsx
@@ -0,0 +1,24 @@
+import React, { useContext, useState } from "react";
+import { QuestionContext } from "ui/contexts/QuestionsContext";
+import { CurrentQuestion } from "./CurrentQuestion";
+
+export const QuestionModal = () => {
+  const {
+    currentQuestion,
+    goToNextQuestion,
+    isSubmited,
+    submitAnswer,
+  } = useContext(QuestionContext);
+  return (
+    <div className="w-116 bg-white rounded-lg px-8 py-14 space-y-8">
+      <CurrentQuestion question={currentQuestion} />
+      <div className="flex justify-end">
+        {!isSubmited ? (
+          <button onClick={submitAnswer}>Submit answer</button>
+        ) : (
+          <button onClick={goToNextQuestion}>Next</button>
+        )}
+      </div>
+    </div>
+  );
+};
