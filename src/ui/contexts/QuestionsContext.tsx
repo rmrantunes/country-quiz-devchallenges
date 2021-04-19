1ea6588f36c9575a1a364b4fb3bc433dd8702304
Rafael Antunes <rafaelmessias710@gmail.com>
2021-04-18 21:11:45 -0400

    feat: add question context functionalities

--- src/ui/contexts/QuestionsContext.tsx
+++ src/ui/contexts/QuestionsContext.tsx
@@ -3,7 +3,11 @@ import { Question } from "types/question";
 
 interface QuestionContextValue {
   currentQuestion: Question;
-  setQuestionIndex: React.Dispatch<React.SetStateAction<number>>;
+  goToNextQuestion(): void;
+  isSubmited: boolean;
+  selectedAnswer: string;
+  setSelectedAnswer: React.Dispatch<React.SetStateAction<string>>;
+  submitAnswer(): void;
 }
 
 export const QuestionContext = createContext({} as QuestionContextValue);
@@ -11,13 +15,39 @@ export const QuestionContext = createContext({} as QuestionContextValue);
 export const QuestionsProvider: React.FC<{ questions: Question[] }> = (
   props
 ) => {
-  const [questionIndex, setQuestionIndex] = useState(1);
+  const [questionIndex, setQuestionIndex] = useState(0);
   const [questions] = useState(props.questions);
-  const currentQuestion = questions[questionIndex];
+  const [isSubmited, setIsSubmited] = useState(false);
   const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [correctAnswers, setCorrectAnswers] = useState<Question[]>([]);
+
+  const currentQuestion = questions[questionIndex];
+
+  function goToNextQuestion() {
+    setIsSubmited(false);
+    setSelectedAnswer("");
+    setQuestionIndex((state) => state + 1);
+  }
+
+  function submitAnswer() {
+    if (!selectedAnswer) return;
+    if (selectedAnswer === currentQuestion.correctAnswer) {
+      setCorrectAnswers((state) => [...state, currentQuestion]);
+    }
+    setIsSubmited(true);
+  }
 
   return (
-    <QuestionContext.Provider value={{ currentQuestion, setQuestionIndex }}>
+    <QuestionContext.Provider
+      value={{
+        currentQuestion,
+        goToNextQuestion,
+        isSubmited,
+        selectedAnswer,
+        setSelectedAnswer,
+        submitAnswer,
+      }}
+    >
       {props.children}
     </QuestionContext.Provider>
   );
