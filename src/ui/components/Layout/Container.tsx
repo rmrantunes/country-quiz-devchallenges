65233d3086a773409f0ad5ef3d94be6bd50635ea
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-06 11:01:12 -0400

    refactor: add Container w/ scss

--- src/ui/components/Layout/Container.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Container: React.FC = (props) => {
-  return (
-    <main className="flex items-center justify-center min-h-full">
-      {props.children}
-    </main>
-  );
-};
