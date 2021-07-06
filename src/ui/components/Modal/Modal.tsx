77b514396946ba255bcdf222b51f28cbd7adea5d
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-06 09:07:17 -0400

    feat: show stats on session finish

--- src/ui/components/Modal/Modal.tsx
+++ src/ui/components/Modal/Modal.tsx
@@ -3,12 +3,21 @@ import { QuestionsContext } from "ui/contexts/QuestionsContext";
 import { QuestionModal } from "./QuestionModal";
 
 export const Modal = () => {
-  const { hasSessionFinished } = useContext(QuestionsContext);
+  const { hasSessionFinished, userAnswersLog, startNewSession } =
+    useContext(QuestionsContext);
   return (
     <div>
       <h1 className="text-4xl font-bold text-white mb-2">COUNTRY QUIZ</h1>
       <div className="w-116 bg-white rounded-lg px-8 py-14 ">
         {!hasSessionFinished && <QuestionModal />}
+        {hasSessionFinished && (
+          <div>
+            <strong>
+              You score {userAnswersLog.filter(Boolean).length}/10
+            </strong>
+            <button onClick={startNewSession}>Jogar novamente</button>
+          </div>
+        )}
       </div>
     </div>
   );
