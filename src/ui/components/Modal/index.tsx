abc128bbd1587bebca89af56677917030e849cfc
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-06 11:44:13 -0400

    feat: implement Progress component

--- src/ui/components/Modal/index.tsx
+++ src/ui/components/Modal/index.tsx
@@ -4,14 +4,21 @@ import QuestionModal from "ui/components/QuestionModal";
 import StatsModal from "ui/components/StatsModal";
 
 import styles from "./styles.module.scss";
+import Progress from "../Progress";
 
 export const Modal = () => {
-  const { hasSessionFinished, userAnswersLog, startNewSession } =
+  const { hasSessionFinished, userAnswersLog, startNewSession, questionIndex } =
     useContext(QuestionsContext);
+
+  const progressPercentage = hasSessionFinished
+    ? (userAnswersLog.filter(Boolean).length / 10) * 100
+    : (questionIndex / 10) * 100;
+
   return (
     <div className={styles.wrapper}>
       <h1>COUNTRY QUIZ</h1>
       <div className={styles.modal}>
+        <Progress percentage={progressPercentage} />
         {!hasSessionFinished && <QuestionModal />}
         {hasSessionFinished && (
           <StatsModal
