d83fb622f87bc41468e6e473a2f4c7008d1eaffa
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-06 12:04:39 -0400

    feat: add winners img to StatsModal

--- src/ui/components/StatsModal/index.tsx
+++ src/ui/components/StatsModal/index.tsx
@@ -10,6 +10,9 @@ const StatsModal = (props: StatsModalProps) => {
   return (
     <div className={styles.stats}>
       <strong>Your score: {props.correctAnswers}/10</strong>
+      {props.correctAnswers >= 7 && (
+        <img src="/images/undraw_winners_ao2o 2.svg" alt="Nice game!" />
+      )}
       <Button onClick={props.onRestart}>Play again</Button>
     </div>
   );
