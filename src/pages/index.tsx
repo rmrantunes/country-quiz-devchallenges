f8085adce5f2b1c069e8e643c5ad25a974fa5f9d
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-06 08:36:50 -0400

    refactor: implement country-quiz-generator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,12 @@
-import Head from "next/head";
-import { InferGetStaticPropsType } from "next";
-
-import countries from "data/countries.json";
-import languages from "data/languages.json";
-import { QuestionGenerator } from "utils/question-generator";
+import { generateQuiz } from "country-quiz-generator";
+import { InferGetServerSidePropsType } from "next";
 
 import { Container } from "ui/components/Layout";
 import { Modal } from "ui/components/Modal";
 import { QuestionsProvider } from "ui/contexts/QuestionsContext";
 
-const questionGenerator = new QuestionGenerator({ countries, languages });
-
-export const getStaticProps = async () => {
-  const questions = questionGenerator.generate(10);
+export const getServerSideProps = async () => {
+  const questions = generateQuiz(10);
 
   return {
     props: {
@@ -22,7 +16,7 @@ export const getStaticProps = async () => {
 };
 
 export default function Home(
-  props: InferGetStaticPropsType<typeof getStaticProps>
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) {
   return (
     <div className="min-h-screen bg-blue-400">
